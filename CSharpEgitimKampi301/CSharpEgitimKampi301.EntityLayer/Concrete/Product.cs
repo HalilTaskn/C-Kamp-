919cfed4fbb8c7d3eb8db9296e134091108a6476
Halil Taşkın <158201694+HalilTaskn@users.noreply.github.com>
2024-10-30 08:33:08 +0300

    Add files via upload

---
+++ CSharpEgitimKampi301/CSharpEgitimKampi301.EntityLayer/Concrete/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Product
+    {
+        public int ProduckId { get; set; }
+
+        public string ProduckName { get; set; }
+
+        public int ProductStock { get; set; }
+
+        public decimal ProductPrice{ get; set; }
+
+        public string ProduckDescription { get; set; }
+    }
+}
+    
